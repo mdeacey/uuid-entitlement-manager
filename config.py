aa912b4592fa8549f95c3ee13e8d35ed879fa2c7
yK2DTzh2 <6YdnN9fT@sector.city>
2024-10-01 16:31:46 +0800

    Issue with not seeing development functions

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ class Config:
     FREE_BALANCE = int(os.getenv('FREE_BALANCE', 10))
     FREE_BALANCE_INTERVAL = int(os.getenv('FREE_BALANCE_INTERVAL', 24 * 60 * 60))
     PAYMENT_URL = os.getenv('PAYMENT_URL', '/webhook?user_uuid={user_uuid}&balance={balance}&status=success')
-    FLASK_ENV = os.getenv('FLASK_ENV', 'production')
+    FLASK_ENV = os.getenv('FLASK_ENV', 'development')
